696f35ceca41adbdd6d00660703742827d7f4f4f
zarin-raim <zarinraim@gmail.com>
2021-11-05 17:45:30 +0100

    BreedsListViewModel.kt exceptions handling

--- app/src/main/java/com/zarinraim/dogsearch/feature/list/BreedsListState.kt
+++
@@ -1,9 +0,0 @@
-package com.zarinraim.dogsearch.feature.list
-
-import com.zarinraim.dogsearch.domain.model.Breeds
-
-data class BreedsListState(
-    val isLoading: Boolean = false,
-    val breeds: Breeds = Breeds(emptyMap()),
-    val error: String = ""
-)
